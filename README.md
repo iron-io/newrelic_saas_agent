2e10f24260028fa094aec17d65db925e00fe32e6
Chad Arimura <carimura@gmail.com>
2013-03-19 21:18:49 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# New Relic IronMQ Stats
+
+1. copy config/sample_config.yml to config/config.yml
+2. fill in config.yml
+3. iron_worker upload newrelic_worker
+4. schedule it
+
---
+++ ironmq_agent.rb
@@ -0,0 +1,100 @@
+# match rabbitmq plugin
+
+require 'rest'
+require 'iron_mq'
+require 'iron_cache'
+require 'yaml'
+
+config = YAML.load_file('config/config.yml')
+
+ironmq = IronMQ::Client.new(
+  token: config['iron']['token'],
+  project_id: config['iron']['project_id'],
+)
+
+ironcache = IronCache::Client.new(
+  token: config['iron']['token'],
+  project_id: config['iron']['project_id'],
+)
+
+cache = ironcache.cache("ironmq_agent_cache")
+
+queues = ironmq.queues.all
+
+components_array = []
+
+# For each queue
+queues.each do |q|
+  size = q.size
+  name = q.name
+
+  puts "\nFound queue: #{name} [#{size} messages]"
+  metric_name_size = "Component/Queue/size[messages]"
+
+  # Add size component
+  components_array << {
+    :name => name,
+    :guid => 'io.iron.mq',
+    :duration => 60,
+    :metrics => {
+      metric_name_size => size,
+    }
+  }
+  puts " Added component #{metric_name_size}"
+
+  # Add rate component
+  key = "#{config['iron']['project_id']}_#{name}_lastsize"
+
+  item = cache.get(key)
+  if item.nil?
+    lastsize = rate = 0
+  else
+    lastsize = item.value
+    rate = size - lastsize
+  end
+  cache.put(key, size)
+
+  puts " Size was #{lastsize}, now #{size}  [Rate #{rate} messages/min]"
+
+  metric_name_rate = "Component/Queue/rate[messages/min]"
+  components_array << {
+    :name => name,
+    :guid => 'io.iron.mq',
+    :duration => 60,
+    :metrics => {
+      metric_name_rate => rate,
+    }
+  }
+  puts " Added component #{metric_name_rate}\n\n"
+end
+
+
+if components_array.empty?
+  puts 'No queues found for this account.'
+else
+
+  body = {
+    :agent => {
+      :host => 'iron.io',
+      :pid => 1,
+      :version => '1.0.0'
+    },
+    :components => components_array
+  }
+
+  headers = {
+    'content-type' => 'application/json',
+    'accept' => 'application/json',
+    'X-License-Key' => config['newrelic_license']
+  }
+
+  puts "Body --> #{body}"
+  puts "Headers --> #{headers}"
+
+  response = Rest::Client.new.post('https://platform-api.newrelic.com/platform/v1/metrics',
+                                   :body => body,
+                                   :headers => headers)
+
+  puts "Response --> #{response.code}"
+
+end
